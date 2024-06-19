1f1c7fc5860a789992315338d80d9f2fd8866e5b
Alvenio Farhan Prayogo <alveniofarhan@gmail.com>
2024-06-19 13:13:43 +0700

    Update Request

--- js/request.js
+++ js/request.js
@@ -1,62 +1,64 @@
-export const HTTP_GET = 'GET';
-export const HTTP_POST = 'POST';
-export const HTTP_PUT = 'PUT';
-export const HTTP_PATCH = 'PATCH';
-export const HTTP_DELETE = 'DELETE';
+export const HTTP_GET = "GET";
+export const HTTP_POST = "POST";
+export const HTTP_PUT = "PUT";
+export const HTTP_PATCH = "PATCH";
+export const HTTP_DELETE = "DELETE";
 
 export const request = (method, path) => {
+  let url = document.querySelector("body").getAttribute("data-url");
+  let req = {
+    method: method,
+    headers: new Headers({
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    }),
+    mode: "cors", // You can change to 'no-cors' if necessary, but it has limitations
+  };
 
-    let url = document.querySelector('body').getAttribute('data-url');
-    let req = {
-        method: method,
-        headers: new Headers({
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+  if (url.slice(-1) === "/") {
+    url = url.slice(0, -1);
+  }
+
+  return {
+    then(...params) {
+      return fetch(url + path, req)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+          }
+          return res.json();
         })
-    };
-
-    if (url.slice(-1) == '/') {
-        url = url.slice(0, -1);
-    }
-
-    return {
-        then(...params) {
-            return fetch(url + path, req)
-                .then((res) => res.json())
-                .then((res) => {
-                    if (res.error) {
-                        throw res.error[0];
-                    }
-
-                    return res;
-                })
-                .then(...params)
-                .catch((err) => alert(err));
-        },
-        download(...params) {
-            return fetch(url + path, req)
-                .then((res) => {
-                    if (res.status === 200) {
-                        return res;
-                    }
-
-                    return null;
-                })
-                .then(...params)
-                .catch((err) => alert(err));
-        },
-        token(token) {
-            if (token.split('.').length === 3) {
-                req.headers.append('Authorization', 'Bearer ' + token);
-                return this;
-            }
-
-            req.headers.append('x-access-key', token);
-            return this;
-        },
-        body(body) {
-            req.body = JSON.stringify(body);
-            return this;
-        },
-    };
+        .then((res) => {
+          if (res.error) {
+            throw new Error(res.error[0]);
+          }
+          return res;
+        })
+        .then(...params)
+        .catch((err) => alert(err.message));
+    },
+    download(...params) {
+      return fetch(url + path, req)
+        .then((res) => {
+          if (res.status === 200) {
+            return res;
+          }
+          throw new Error(`HTTP error! status: ${res.status}`);
+        })
+        .then(...params)
+        .catch((err) => alert(err.message));
+    },
+    token(token) {
+      if (token.split(".").length === 3) {
+        req.headers.set("Authorization", "Bearer " + token);
+      } else {
+        req.headers.set("x-access-key", token);
+      }
+      return this;
+    },
+    body(body) {
+      req.body = JSON.stringify(body);
+      return this;
+    },
+  };
 };
